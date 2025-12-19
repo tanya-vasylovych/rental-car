d885f70dbadff6e776a37c6e81cd17dbdc81eaca
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-12-19 23:17:45 +0200

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,37 @@
-import css from "./page.module.css";
+"use client";
+import { motion } from "framer-motion";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <motion.h1
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        style={{ fontSize: "5rem", color: "#333" }}
+      >
+        404
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        style={{
+          fontSize: "1.5rem",
+          color: "#777",
+          marginTop: "1rem",
+          marginLeft: "1rem",
+        }}
+      >
         Sorry, the page you are looking for does not exist.
-      </p>
+      </motion.p>
     </div>
   );
 };
